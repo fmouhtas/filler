56f3d9da7d23c52cbd33e3f7df9232c89d587711
Farice MOUHTASEB <fmouhtas@student.42.fr>
2018-06-26 09:48:29 +0200

    visu

--- visu/srcs/main.c
+++ visu/srcs/main.c
@@ -6,11 +6,33 @@
 /*   By: fmouhtas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/25 17:29:46 by fmouhtas          #+#    #+#             */
-/*   Updated: 2018/06/25 18:12:19 by fmouhtas         ###   ########.fr       */
+/*   Updated: 2018/06/26 09:48:02 by fmouhtas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "visu.h"
+
+static int	draw_map(t_env *env)
+{
+	int		i;
+	int		j;
+	char	*line;
+
+	i = 0;
+	while (i < env->height)
+	{
+		if (get_next_line(1, &line) != 1)
+			return (1);
+		j = 0;
+		while (j < env->width)
+		{
+			draw_square(env, i, j);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
 
 static int	get_size(t_env *env)
 {
@@ -24,6 +46,7 @@ static int	get_size(t_env *env)
 	while (line[i] != ' ')
 		i++;
 	env->width = ft_atoi(line + i);
+	free(line);
 	return (1);
 }
 
@@ -32,12 +55,11 @@ int		main(void)
 	t_env	env;
 	
 	env.mlx = mlx_init();
-	env.win = mlx_new_window(env.mlx, 600, 800, "VISU FILLER");
 	skip_line(9);
 	get_size(&env);
+	env.win = mlx_new_window(env.mlx, env.width * 5, env.height * 5, "VISU FILLER");
 	while (1)
 	{
-		if (!get_size(&env))
-			return (0);
+		draw_map(&env);
 	}
 }
